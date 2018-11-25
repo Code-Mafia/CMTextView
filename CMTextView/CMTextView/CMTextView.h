21b3949d6ec3f19ddfcf517404678a7911e8f10a
Rajmani Kushwaha <Rajmanikush@gmail.com>
2018-11-26 01:39:10 +0530

    CMTextView framework created

---
+++ CMTextView/CMTextView/CMTextView.h
@@ -0,0 +1,19 @@
+//
+//  CMTextView.h
+//  CMTextView
+//
+//  Created by Rajmani on 26/11/18.
+//  Copyright © 2018 Code Mafia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CMTextView.
+FOUNDATION_EXPORT double CMTextViewVersionNumber;
+
+//! Project version string for CMTextView.
+FOUNDATION_EXPORT const unsigned char CMTextViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CMTextView/PublicHeader.h>
+
+
---
+++ CMTextView/CMTextView/CMTextView.swift
@@ -0,0 +1,140 @@
+//
+//  CMTextView.swift
+//  CMTextViewExample
+//
+//  Created by Rajmani on 25/11/18.
+//  Copyright © 2018 Code Mafia. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CMTextView: UITextView {
+    
+    private var placeholderLabel: UILabel?
+    
+    @IBInspectable var placeholderText: String = "" {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var placeholderColor: UIColor = .lightGray {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var cornerRadius: Double {
+        get {
+            return Double(self.layer.cornerRadius)
+        }set {
+            self.layer.cornerRadius = CGFloat(newValue)
+        }
+    }
+    @IBInspectable var borderWidth: Double {
+        get {
+            return Double(self.layer.borderWidth)
+        }
+        set {
+            self.layer.borderWidth = CGFloat(newValue)
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.borderColor!)
+        }
+        set {
+            self.layer.borderColor = newValue?.cgColor
+        }
+    }
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.shadowColor!)
+        }
+        set {
+            self.layer.shadowColor = newValue?.cgColor
+        }
+    }
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return self.layer.shadowOpacity
+        }
+        set {
+            self.layer.shadowOpacity = newValue
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialize()
+    }
+    
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        initialize()
+    }
+    
+    
+    override func prepareForInterfaceBuilder() {
+        initialize()
+    }
+    
+    func initialize() {
+        
+        self.layoutGUI()
+        NotificationCenter.default.addObserver(self, selector: .textDidChanged, name: UITextView.textDidChangeNotification, object: self)
+    }
+    
+    // MARK: - Notification center
+    
+    @objc func textDidChange(notification:Notification) {
+        
+        guard let textView = notification.object as? CMTextView else {
+            return
+        }
+        
+        if textView == self {
+            self.layoutGUI()
+        }
+    }
+    
+    // MARK: - Layout Method
+    
+    func layoutGUI() {
+        placeholderLabel?.alpha = self.text.count > 0 || placeholderText.count == 0 ? 0 : 1
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        
+        if placeholderText.count > 0 {
+            if placeholderLabel == nil {
+                let placeholderLabel =  UILabel(frame: CGRect(x: 8, y: 8, width: self.bounds.size.width - 16, height: 0))
+                placeholderLabel.lineBreakMode = .byWordWrapping
+                placeholderLabel.numberOfLines = 0
+                placeholderLabel.font = self.font
+                placeholderLabel.backgroundColor = .clear
+                placeholderLabel.alpha = 0
+                self.placeholderLabel = placeholderLabel
+                self.addSubview(placeholderLabel)
+            }
+            
+            placeholderLabel?.text = placeholderText
+            placeholderLabel?.textColor = placeholderColor
+            placeholderLabel?.sizeToFit()
+            self.sendSubviewToBack(placeholderLabel!)
+        }
+        
+        layoutGUI()
+        
+        super.draw(rect)
+    }
+}
+
+private extension Selector {
+    static let textDidChanged = #selector(CMTextView.textDidChange(notification:))
+}
